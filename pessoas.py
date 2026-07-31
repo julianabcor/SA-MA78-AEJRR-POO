b4032c3e65fce6e488594024e841ed697bf542bd
EMILLY MIRANDA DREHMER <emilly_drehmer@estudante.sesisenai.org.br>
2026-07-30 21:17:24 -0300

    feat(pessoas): Adiciona entidade cliente

--- pessoas.py
+++ pessoas.py
@@ -7,3 +7,14 @@ class ProprietarioRepositorio(RepositorioBase):
     campo_nome = "nome"
     msg_erro_atualizar = "este CPF/CNPJ já está cadastrado para outro proprietário."
     msg_erro_deletar = "ele possui Imóveis cadastrados em seu nome."
+
+
+class ClienteRepositorio(RepositorioBase):
+    tabela = "Cliente"
+    classe_entidade = Cliente
+    pk = "id_cliente"
+    colunas_inserir = ["nome", "cpf", "telefone", "email"]
+    nome_exibicao = "Cliente"
+    campo_nome = "nome"
+    msg_erro_atualizar = "este CPF já está cadastrado para outro cliente."
+    msg_erro_deletar = "ele possui registros de Visitas, Vendas ou Aluguéis vinculados."
